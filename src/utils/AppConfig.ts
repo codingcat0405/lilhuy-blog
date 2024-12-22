6d7d6a9d88cc3ff4cd7292625932a9c228f3ae8b
lilhuy0405 <luuhuy040501.travail@gmail.com>
2024-12-22 21:31:51 +0700

    init project

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,8 @@
+export const AppConfig = {
+	site_name: 'Lilhuy Blog',
+	title: 'Lilhuy blog | My personal blog',
+	description: 'Where I write about my thoughts and experiences',
+	author: 'lilhuy',
+	locale_region: 'vi_VN',
+	locale: 'vi'
+};
